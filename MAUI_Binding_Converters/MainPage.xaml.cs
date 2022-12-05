43e3cf77f4e460a8f68165f022c3b5c5017d744c
maheshsabnis <sabnis_m@hotmail.com>
2022-12-05 08:52:33 +0530

    Converter

---
+++ MAUI_Binding_Converters/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MAUI_Binding_Converters
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
