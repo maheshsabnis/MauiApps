531771ecaaf37f3be07733978d6dd02d6583d794
maheshsabnis <sabnis_m@hotmail.com>
2022-12-05 08:40:34 +0530

    String Formatting

---
+++ MAUI_StringFormatting/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUI_StringFormatting
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MAUI_StringFormatting/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUI_StringFormatting
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
